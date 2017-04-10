64b540580f604b95002f7ba60b3f832202dd69b2
bohanzhang <bohanzhang@foxmail.com>
2017-04-10 23:11:06 +0800

    test iview

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
+  <Slider v-model="value" range></Slider>
     <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import {Slider} from 'iview'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {Slider},
+  data () {
+    return {
+      value: [20, 50]
+    }
+  }
 }
 </script>
 
